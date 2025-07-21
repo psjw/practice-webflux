29e9b45d0e1cea78ccd3d134dd269c205cdd6a3f
psjw <dlrudtn108@gmail.com>
2025-07-21 13:15:10 +0900

    리액티브 프로그래밍이란?

---
+++ src/main/java/com/example/webflux/controller/ReactiveProgrammingExampleController.java
@@ -0,0 +1,53 @@
+package com.example.webflux.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/reactive")
+public class ReactiveProgrammingExampleController {
+
+    //1~9까지 출력하는 api
+    @GetMapping("/onenine/list")
+    public List<Integer> produceOneToNine() {
+        List<Integer> sink = new ArrayList<>();
+        for (int i = 1; i < 10; i++) {
+            try {
+                Thread.sleep(500); //0.5초동안 Sleep
+            } catch (Exception e) {
+
+            }
+            sink.add(i);
+        } //총 4.5초 소요 -> 요건사항 : 데이터가 처리 될때마다 바로 보여주세요
+        return sink;
+    }
+
+    //Flux와 Mono를 사용하면 아주 쉽게 리액티브한 프로그래밍이 가능
+    @GetMapping("/onenine/flux")
+    public Flux<Integer> produceOneToNineFlux() {
+        return Flux.create(sink -> {
+            for (int i = 1; i < 10; i++) {
+                try {
+                    Thread.sleep(500); //0.5초동안 Sleep
+                } catch (Exception e) {
+
+                }
+                sink.next(i);
+            } //총 4.5초 소요 -> 요건사항 : 데이터가 처리 될때마다 바로 보여주세요
+            sink.complete();//사용후 닫아줌
+        });
+    }
+    //리액티브 스트림 구현체 Flux, Mono를 사용하여 발생하는 데이터를 바로바로 리액티브하게 처리
+    //비동기로동작 -> 논 블로킹하게 동작 해야한다. -> Thread.sleep이 블로킹하므로 우회하여 회피해야함
+
+    //리액티브 프로그래밍 필수 요소
+    //1. 데이터가 준비될 때 마다 바로바로 리액티브하게 처리
+    //  > 리액티브 스트림 구현체 Flux, Mono를 사용하여 발생하는 데이터 바로바로 처리
+    //2. 로직을 짤 때는 반드시 논 블로킹하게 짜야함
+    //  > 이를 위해 비동기 프로그래밍이 필요
+}
